07f2ae41f24750394b9fda2b01b036f5d3af718d
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-12-02 19:39:43 +0800

    Create configure.sh

---
+++ scripts/configure.sh
@@ -0,0 +1,27 @@
+apt-get update -qq && apt-get upgrade -qqy
+apt-get install -qqy git sudo wget curl zsh vim nano tmux tree htop screen rsync gnupg ca-certificates uuid-runtime tzdata openssh-server lrzsz xz-utils
+wget -q https://build-scripts.project-openwrt.eu.org/init_build_environment.sh
+sed -i 's/apt install/apt-get install/g' init_build_environment.sh
+sed -i 's/apt clean/apt-get clean/g' init_build_environment.sh
+sed -i 's/apt full-upgrade/apt-get full-upgrade/g' init_build_environment.sh
+sed -i '/Chinese/d' init_build_environment.sh
+chmod +x init_build_environment.sh
+./init_build_environment.sh
+rm init_build_environment.sh
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+mkdir /var/run/sshd
+useradd -m -G sudo -s /usr/bin/zsh admin
+echo 'admin:admin' | chpasswd
+echo 'admin ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/admin
+chmod 440 /etc/sudoers.d/admin
+curl -fsSL git.io/tmate.sh | bash
+curl -fsSL git.io/gotop.sh | bash -s install
+curl -fsSL git.io/bashtop.sh | bash -s install
+mkdir -p /home/admin
+cd /home/admin
+mkdir -p /home/admin/.ssh
+chmod 700 /home/admin/.ssh
+HOME="/home/admin" && \
+curl -fsSL git.io/oh-my-zsh.sh | bash
+curl -fsSL git.io/oh-my-tmux.sh | bash
+chown -R admin:admin /home/admin
